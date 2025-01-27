ce13211c11f9200a58dce182a87f721660b4771e
BaronMac09 <baron.mac08@gmail.com>
2025-01-27 14:29:57 +0700

    Finished Creating migrations and Updating database

--- AuthAPIDemo/Program.cs
+++ AuthAPIDemo/Program.cs
@@ -1,4 +1,8 @@
+using AuthAPIDemo.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("MySqlConnection");
 
 // Add services to the container.
 
@@ -6,6 +10,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseMySQL(connectionString);
+});
 
 var app = builder.Build();
 
